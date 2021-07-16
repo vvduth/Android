fdce8ba926ea1c7b3586606dce69389c37d8c837
vvduth <ducthai060501@gmail.com>
2021-07-16 18:24:42 +0300

    "Test some Xml files"

--- app/src/main/java/org/mycode/uibasic/MainActivity.java
+++ app/src/main/java/org/mycode/uibasic/MainActivity.java
@@ -1,11 +1,15 @@
 package org.mycode.uibasic;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.os.SystemClock;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -24,71 +28,43 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-   private ListView citiesList ;
-   private Spinner studentsSpinner;
+    private ListView citiesList;
+    private Spinner studentsSpinner;
+    private TextView txtHello;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        citiesList = findViewById(R.id.citiesList);
-        studentsSpinner  = findViewById(R.id.studentsSpinner);
+        txtHello = findViewById(R.id.txtHello);
+        txtHello.setText(getString(R.string.hello)); //Import from string.xml
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater(); // pass the menu in parameter to the main menu
+        inflater.inflate(R.menu.main_menu, menu); // embed all the item in the main_menu file to the UI
+        return true;
+    }
 
 
-       /* final ArrayList<String> students = new ArrayList<>();
-        students.add("Duc");
-        students.add("Sarah");
-        students.add("Mason");
-        students.add("Kyle");
-        students.add("Raheem");
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // the item user choose
 
-        ArrayAdapter<String> studentsAdapter = new ArrayAdapter<>(
-                this, android.R.layout.simple_spinner_dropdown_item,
-                students
-        );
+        switch (item.getItemId()) {
+            case R.id.settings_menu: // if the id of the item is setting:menu
+                Toast.makeText(this, "Setting selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.alarm_menu: // if the id of the item is setting:menu
+                Toast.makeText(this, "Alarm selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item); // pass the job to the parents class
 
-        studentsSpinner.setAdapter(studentsAdapter);  */
-
-        final ArrayList<String> cities = new ArrayList<>();
-        cities.add("New York");
-        cities.add("London");
-        cities.add("Tampere");
-        cities.add("Manchester");
-        cities.add("Sydney");
-        cities.add("Kansas");
-
-
-
-
-        //Pass the data from Array list to the listview
-        ArrayAdapter<String> citiesAdapter = new ArrayAdapter<>(
-             this,
-             android.R.layout.simple_list_item_1,
-             cities
-     );
-
-     citiesList.setAdapter(citiesAdapter);
-
-     studentsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-         @Override
-         public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this,studentsSpinner.getSelectedItem().toString() + " Selected", Toast.LENGTH_SHORT).show();
-         }
-
-         @Override
-         public void onNothingSelected(AdapterView<?> parent) {
-
-         }
-     });
-
-     citiesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-         @Override
-         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-             Toast.makeText(MainActivity.this, cities.get(position)+ " Selected",Toast.LENGTH_SHORT).show();
-         }
-     });
-
+        }
 
     }
+
 }
