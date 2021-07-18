4941cb3286732c627a11740ef224973eff1f061b
vvduth <ducthai060501@gmail.com>
2021-07-18 17:39:37 +0300

    "Materials Degisn"

--- app/src/main/java/org/mycode/uibasic/MainActivity.java
+++ app/src/main/java/org/mycode/uibasic/MainActivity.java
@@ -24,46 +24,27 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView citiesList;
-    private Spinner studentsSpinner;
-    private TextView txtHello;
+    private FloatingActionButton fab;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(v -> Toast.makeText(MainActivity.this, "Fab Clicked", Toast.LENGTH_SHORT).show());
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater(); // pass the menu in parameter to the main menu
-        inflater.inflate(R.menu.main_menu, menu); // embed all the item in the main_menu file to the UI
-        return true;
-    }
 
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        // the item user choose
 
-        switch (item.getItemId()) {
-            case R.id.settings_menu: // if the id of the item is setting:menu
-                Toast.makeText(this, "Setting selected", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.alarm_menu: // if the id of the item is setting:menu
-                Toast.makeText(this, "Alarm selected", Toast.LENGTH_SHORT).show();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item); // pass the job to the parents class
 
-        }
-
-    }
 
 }
